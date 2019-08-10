1abf6b9a70193e806e6817101548a87a2fc28640
Froilan Irizarry <froilan@froilanirizarry.me>
2019-08-09 23:32:28 -0400

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    let paths = {
+      '/': { page: '/' }
+    };
+
+    for (let i = 0; i < 889; i++) {
+      let pageNumber = `${i + 1}`;
+      paths[`/page/${pageNumber}`] = {
+        page: '/page/[pageNumber]',
+        query: { pageNumber }
+      };
+    }
+
+    return paths;
+  }
+});
